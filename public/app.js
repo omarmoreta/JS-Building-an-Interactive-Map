78d2cd2ee6947d276ae76b58dda9d07bc5a446a1
Omar <o.moreta@gmail.com>
2022-08-25 20:14:55 -0400

    updated leaflet to new version

--- public/app.js
+++ public/app.js
@@ -1,8 +1,5 @@
 // create map
-const myMap = L.map("map", {
-  center: [48.868672, 2.34213],
-  zoom: 12,
-});
+const myMap = L.map("map").setView([48.868672, 2.34213], 13);
 
 // add openstreetmap tiles
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -27,23 +24,17 @@ let prS = L.marker([48.866200610611926, 2.352236247419453], {
 }).bindPopup("Réaumur-Sébastopol");
 
 // // metro station markers
-let rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup(
-  "Réaumur-Sébastopol"
-);
-let sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup(
-  "Strasbourg-Saint-Denis"
-);
-let sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup(
-  "Sentier"
-);
-let bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup(
-  "Bourse"
-);
-let qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup(
-  "Quatre Septembre"
-);
-let gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup(
-  "Grands Boulevards"
-);
+let rS = L.marker([48.866200610611926, 2.352236247419453]);
+rS.bindPopup("Réaumur-Sébastopol");
+let sSD = L.marker([48.869531786321566, 2.3528590208055196]);
+sSD.bindPopup("Strasbourg-Saint-Denis");
+let sentier = L.marker([48.8673721067762, 2.347107922912739]);
+sentier.bindPopup("Sentier");
+let bourse = L.marker([48.86868503971672, 2.3412285142058167]);
+bourse.bindPopup("Bourse");
+let qS = L.marker([48.869560129483226, 2.3358638645569543]);
+qS.bindPopup("Quatre Septembre");
+let gB = L.marker([48.871282159004856, 2.3434818588892714]);
+gB.bindPopup("Grands Boulevards");
 
 let stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap);
